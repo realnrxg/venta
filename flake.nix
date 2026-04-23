e19f3e0ae12df34ce521bfa080ae2acd39e52a35
nrxg <120276921+realnrxg@users.noreply.github.com>
2026-04-23 21:06:28 +0200

    Update to 1.3.1

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      ver = "1.3";
+      ver = "1.3.1";
     in {
       packages.default = pkgs.stdenv.mkDerivation {
         pname = "venta";
